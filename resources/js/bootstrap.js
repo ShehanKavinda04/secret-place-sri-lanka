43247cf1e0e521d8415dda4db140813e1c6e6a44
skavinda <kavinda20000602@gmail.com>
2026-09-08 20:54:25 +0530

    error fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,8 +10,8 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
+    key: import.meta.env.VITE_REVERB_APP_KEY || import.meta.env.VITE_PUSHER_APP_KEY || 'local_key',
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
